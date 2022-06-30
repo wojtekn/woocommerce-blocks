eceedb085d6f0eb76aa448f5af1079cbfe91bd51
Niels Lange <info@nielslange.de>
2022-06-30 15:35:20 +0200

    Load Cross-Sells block children on frontend

--- assets/js/blocks/cart/inner-blocks/cart-cross-sells-block/frontend.tsx
+++ assets/js/blocks/cart/inner-blocks/cart-cross-sells-block/frontend.tsx
@@ -1,6 +1,10 @@
-/**
- * Internal dependencies
- */
-import Block from './block';
+interface Props {
+	children?: JSX.Element | JSX.Element[];
+	className?: string;
+}
 
-export default Block;
+const FrontendBlock = ( { children, className = '' }: Props ): JSX.Element => {
+	return <div className={ className }>{ children }</div>;
+};
+
+export default FrontendBlock;
