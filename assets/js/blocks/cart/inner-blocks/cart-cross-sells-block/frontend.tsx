4ba8701f65ff408f8e4a4e64c730a2d825df9f52
Niels Lange <info@nielslange.de>
2022-07-25 16:37:10 +0200

    Hide title if no Cross-Sells products are available

--- assets/js/blocks/cart/inner-blocks/cart-cross-sells-block/frontend.tsx
+++ assets/js/blocks/cart/inner-blocks/cart-cross-sells-block/frontend.tsx
@@ -1,9 +1,23 @@
+/**
+ * External dependencies
+ */
+import { useStoreCart } from '@woocommerce/base-context/hooks';
+
 interface Props {
 	children?: JSX.Element | JSX.Element[];
 	className?: string;
 }
 
-const FrontendBlock = ( { children, className = '' }: Props ): JSX.Element => {
+const FrontendBlock = ( {
+	children,
+	className = '',
+}: Props ): JSX.Element | null => {
+	const { crossSellsProducts, cartIsLoading } = useStoreCart();
+
+	if ( cartIsLoading || crossSellsProducts.length < 1 ) {
+		return null;
+	}
+
 	return <div className={ className }>{ children }</div>;
 };
 
